e8c7204f8e2dce72339f0f71f164363abe671464
stepin104447 <69075631+stepin104447@users.noreply.github.com>
2020-10-06 19:06:55 +0530

    Update test_medical_store.c

--- MiniProject_C/3_Implementation/test/test_medical_store.c
+++ MiniProject_C/3_Implementation/test/test_medical_store.c
@@ -52,57 +52,58 @@ void test_add_medicine(void) {
   CU_ASSERT(1 == add_medicine(101,1,"medicine 101","supplier 101",1001,"12341001","supplier1001@gmail.com","company 101",100.00,20,2,3));
   CU_ASSERT(1 == add_medicine(102,1,"medicine 102","supplier 102",1002,"12341002","supplier1002@gmail.com","company 102",150.00,30,4,1));
   CU_ASSERT(1 == add_medicine(103,2,"medicine 103","supplier 103",1003,"12341003","supplier1003@gmail.com","company 103",200.00,40,2,4));
+   CU_ASSERT(1 == add_medicine(104,3,"medicine 104","supplier 104",1004,"12341004","supplier1004@gmail.com","company 104",120.00,44,3,2));
+   CU_ASSERT(1 == add_medicine(105,4,"medicine 105","supplier 105",1005,"12341005","supplier1005@gmail.com","company 105",140.00,25,5,4));
   
   /* Dummy fail*/
- /* CU_ASSERT(1500 == add(750, 7500));*/
 }
 
 void test_dispose_medicine(void) {
   CU_ASSERT(1 == dispose_medicine(101));
- // CU_ASSERT(1 == dispose_medicine(103));
   
   /* Dummy fail*/
-  /*CU_ASSERT(1 == subtract(1000, 900));*/
+  
 }
 
 void test_search_medicine(void) {
-  CU_ASSERT(1 == search_medicine('1',102,"medicine 102"));
-  CU_ASSERT(1 == search_medicine('2',102,"medicine 102"));
+  CU_ASSERT(1 == search_medicine('1',102,"medicine 102"));  //search with id
+  CU_ASSERT(1 == search_medicine('2',102,"medicine 102"));  //search with name
   
   /* Dummy fail*/
-  /*CU_ASSERT(2 == multiply(2, 5));*/
+  
 }
 
 void test_sell_medicine(void) {
-  CU_ASSERT(1 == sell_medicine(102,'1',5,102,102,"12341021","customer 1021"));
-  CU_ASSERT(1 == sell_medicine(102,'3',5,102,102,"12341021","customer 1021"));
-  CU_ASSERT(1 == sell_medicine(102,'2',5,102,102,"12341021","customer 1021"));
+  CU_ASSERT(1 == sell_medicine(102,'1',5,102,102,"12341021","customer 1021")); //sell medicine
+  CU_ASSERT(1 == sell_medicine(102,'3',5,102,102,"12341021","customer 1021"));  // list sold medicine
+  CU_ASSERT(1 == sell_medicine(102,'2',5,102,102,"12341021","customer 1021")); // search sold medicine
+  CU_ASSERT(1 == sell_medicine(102,'4',5,102,102,"12341021","customer 1021")); // remove sold medicine
   
   /* Dummy fail*/
- /* CU_ASSERT(3 == divide(2, 2));*/
+ 
 }
                               
 void test_list_medicine(void) {
   CU_ASSERT(1 == list_medicine());
   
   /* Dummy fail*/
- /* CU_ASSERT(3 == divide(2, 2));*/
+ 
 }
                               
 void test_edit_medicine(void) {
   CU_ASSERT(1 == edit_medicine(102,"medicine 102","supplier 102",1002,"12341002","company 102","supplier1001@gmail.com",50,1,5));
-  //CU_ASSERT(1 == edit_medicine(103,"medicine 103","supplier 103",1003,"12341003","company 103","supplier1001@gmail.com",70,2,5));
+  
   
   /* Dummy fail*/
- /* CU_ASSERT(3 == divide(2, 2));*/
+ 
 }
                                
 void test_supplier_info(void) {
-  CU_ASSERT(1 == supplier_info('1',1001));
- CU_ASSERT(1 == supplier_info('2',1001));
+  CU_ASSERT(1 == supplier_info('1',1001)); //list of suppliers
+ CU_ASSERT(1 == supplier_info('2',1001));  // search supplier
   
   /* Dummy fail*/
- /* CU_ASSERT(3 == divide(2, 2));*/
+ 
 }
 
 
