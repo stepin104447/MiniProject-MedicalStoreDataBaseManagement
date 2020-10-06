bc9462fdecfbd3dbaf9fb8cd07cc94faf31f056f
stepin104447 <69075631+stepin104447@users.noreply.github.com>
2020-10-06 18:33:23 +0530

    Update test_medical_store.c

--- MiniProject_C/3_Implementation/test/test_medical_store.c
+++ MiniProject_C/3_Implementation/test/test_medical_store.c
@@ -59,7 +59,7 @@ void test_add_medicine(void) {
 
 void test_dispose_medicine(void) {
   CU_ASSERT(1 == dispose_medicine(101));
-  //CU_ASSERT(1 == dispose_medicine(103));
+  CU_ASSERT(1 == dispose_medicine(103));
   
   /* Dummy fail*/
   /*CU_ASSERT(1 == subtract(1000, 900));*/
@@ -67,7 +67,7 @@ void test_dispose_medicine(void) {
 
 void test_search_medicine(void) {
   CU_ASSERT(1 == search_medicine('1',102,"medicine 102"));
- // CU_ASSERT(1 == search_medicine('2',102,"medicine 102"));
+  CU_ASSERT(1 == search_medicine('2',102,"medicine 102"));
   
   /* Dummy fail*/
   /*CU_ASSERT(2 == multiply(2, 5));*/
@@ -75,7 +75,7 @@ void test_search_medicine(void) {
 
 void test_sell_medicine(void) {
   CU_ASSERT(1 == sell_medicine(102,'1',5,102,102,"12341021","customer 1021"));
-  //CU_ASSERT(1 == sell_medicine(102,'3',5,102,102,"12341021","customer 1021"));
+  CU_ASSERT(1 == sell_medicine(102,'3',5,102,102,"12341021","customer 1021"));
   
   /* Dummy fail*/
  /* CU_ASSERT(3 == divide(2, 2));*/
@@ -90,7 +90,7 @@ void test_list_medicine(void) {
                               
 void test_edit_medicine(void) {
   CU_ASSERT(1 == edit_medicine(102,"medicine 102","supplier 102",1002,"12341002","company 102","supplier1001@gmail.com",50,1,5));
-  //CU_ASSERT(1 == edit_medicine(103,"medicine 103","supplier 103",1003,"12341003","company 103","supplier1001@gmail.com",70,2,5));
+  CU_ASSERT(1 == edit_medicine(103,"medicine 103","supplier 103",1003,"12341003","company 103","supplier1001@gmail.com",70,2,5));
   
   /* Dummy fail*/
  /* CU_ASSERT(3 == divide(2, 2));*/
@@ -98,7 +98,7 @@ void test_edit_medicine(void) {
                                
 void test_supplier_info(void) {
   CU_ASSERT(1 == supplier_info('1',1001));
- // CU_ASSERT(1 == supplier_info('2',1001));
+ CU_ASSERT(1 == supplier_info('2',1001));
   
   /* Dummy fail*/
  /* CU_ASSERT(3 == divide(2, 2));*/
